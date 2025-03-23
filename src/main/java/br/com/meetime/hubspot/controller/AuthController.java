d32cdd2d47cde5b98f6368b9684bc5ec8f99cdce
Giovanni R Santos <giovannirodrigues.2012@gmail.com>
2025-03-23 17:56:20 -0300

    Realizando consumo do authService

--- src/main/java/br/com/meetime/hubspot/controller/AuthController.java
+++ src/main/java/br/com/meetime/hubspot/controller/AuthController.java
@@ -5,15 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.meetime.hubspot.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
     @GetMapping("/authorize")
     public ResponseEntity<String> getAuthorizationURL() {
-        //Chamar service
-        return ResponseEntity.ok("Provisorio");
+        String url = authService.getAuthorizationURL();
+        return ResponseEntity.ok(url);
     }
 }
